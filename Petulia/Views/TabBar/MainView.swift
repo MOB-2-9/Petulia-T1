a233cf9bf25a87032af6c4be46b0d28691027b2c
Anika Morris <anikamorris11@gmail.com>
2021-04-15 18:20:00 -0500

    changed icon for organization tab

--- Petulia/Views/TabBar/MainView.swift
+++ Petulia/Views/TabBar/MainView.swift
@@ -18,11 +18,11 @@ struct MainView: View {
                 icon: { Image(systemName: "house.fill") }
               )
             }
-          SettingsView()
+          OrganizationsView()
             .tabItem {
               Label(
-                title: { Text("Settings") },
-                icon: { Image(systemName: "gear") }
+                title: { Text("Organizations") },
+                icon: { Image(systemName: "person.3.fill") }
               )
             }
         }
