fd689bb443e2c1bf05bd55efdf6bd4e19ba594fc
Henry Calderon <theceintiant@gmail.com>
2021-05-06 19:10:26 -0700

    The addition has been added for the items of filtering

--- Petulia/Views/Home/Additional/DetailedFilterView.swift
+++ Petulia/Views/Home/Additional/DetailedFilterView.swift
@@ -10,24 +10,23 @@ import SwiftUI
 
 //MARK: The Filter View
 struct DetailFilterView: View {
-//  var primaryAction: (() -> Void)?
-//  var settingsAction: (() -> Void)?
+  //  var primaryAction: (() -> Void)?
+  //  var settingsAction: (() -> Void)?
   
   @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
   var body: some View {
-//    ScrollView(.horizontal, showsIndicators: false) {
-      VStack(alignment: .leading) {
-        FilterBreed()
-        FilterAge()
-        FilterSize()
-        FilterGender()
-        FilterLocation()
-        Button("Done",action: {self.presentationMode.wrappedValue.dismiss()})
-      }
-      .lineSpacing(20)
-      .frame(maxWidth: .infinity)
-      .padding()
-//    }
+    VStack(alignment: .leading) {
+      FilterBreed()
+      FilterAge()
+      FilterSize()
+      FilterGender()
+      FilterLocation()
+      Button("Done",action: {self.presentationMode.wrappedValue.dismiss()})
+    }
+    .navigationBarTitle("Filters")
+    .lineSpacing(20)
+    .frame(maxWidth: .infinity)
+    .padding()
   }
 }
 
@@ -35,57 +34,124 @@ struct DetailFilterView: View {
 struct FilterBreed: View {
   var breeds = ["Breed1", "Breed2", "Breed3"]
   @State private var selectedBreed = 0
+  @State private var toggleAdd = false
   var body: some View {
-    Text("Breed")
-      .font(.title2)
-    DropDownPicker(title: "Select Breed", selection: $selectedBreed, options: breeds)
+    VStack{
+      HStack{
+        Text("Breed")
+          .font(.title2)
+        Button(action: {
+          toggleAdd.toggle()
+        }){
+          if toggleAdd{
+            Image(systemName: "minus")
+          }else{
+            Image(systemName: "plus")
+          }
+        }
+      }
+      DropDownPicker(title: "Select Breed", selection: $selectedBreed, options: breeds)
+    }
   }
 }
 
 struct FilterAge: View {
   @State var sliderValue: Double = 0
+  @State private var toggleAdd = false
   var body: some View {
-    Text("Age")
-      .font(.title2)
     VStack{
-      Slider(value: $sliderValue, in: 0...30, step: 1)
-      Text("Age up to \(sliderValue, specifier: "%.1f")")
-    }.padding()
+      HStack{
+        Text("Age")
+          .font(.title2)
+        Button(action: {
+          toggleAdd.toggle()
+        }){
+          if toggleAdd{
+            Image(systemName: "minus")
+          }else{
+            Image(systemName: "plus")
+          }
+        }
+      }
+      VStack{
+        Slider(value: $sliderValue, in: 0...30, step: 1)
+        Text("Age up to \(sliderValue, specifier: "%.1f")")
+      }.padding()
+      
+    }
   }
 }
 
 struct FilterSize: View {
   var sizes = ["small", "medium", "large"]
   @State private var selectedSize = 0
+  @State private var toggleAdd = false
   var body: some View {
-    Text("Some Size")
-      .font(.title2)
-    DropDownPicker(title: "Pet Size", selection: $selectedSize, options: sizes)
+    VStack{
+      HStack{
+        Text("Animal Size")
+          .font(.title2)
+        Button(action: {
+          toggleAdd.toggle()
+        }){
+          if toggleAdd{
+            Image(systemName: "minus")
+          }else{
+            Image(systemName: "plus")
+          }
+        }
+      }
+      DropDownPicker(title: "Pet Size", selection: $selectedSize, options: sizes)
+    }
   }
 }
 
 struct FilterGender: View {
   @State private var selectedGender = 0
+  @State private var toggleAdd = false
   var body: some View {
-    Text("Gender")
-      .font(.title2)
-    DropDownPicker(title: "Gender", selection: $selectedGender, options: ["Male","Female"])
+    VStack{
+      HStack{
+        Text("Gender")
+          .font(.title2)
+        Button(action: {
+          toggleAdd.toggle()
+        }){
+          if toggleAdd{
+            Image(systemName: "minus")
+          }else{
+            Image(systemName: "plus")
+          }
+        }
+      }
+      DropDownPicker(title: "Gender", selection: $selectedGender, options: ["Male","Female"])
+    }
   }
 }
 
 struct FilterLocation: View {
   @State private var location: String = ""
+  @State private var toggleAdd = false
   var body: some View {
-    Text("Location")
-      .font(.title2)
-    TextField("Location",
-              text: $location)
-      .font(.headline)
-      .multilineTextAlignment(.center)
-      .frame(maxWidth: 100)
-      .padding(.vertical, 8)
+    VStack{
+      HStack(){
+        Text("Location")
+          .font(.title2)
+        Button(action: {
+          toggleAdd.toggle()
+        }){
+          if toggleAdd{
+            Image(systemName: "minus")
+          }else{
+            Image(systemName: "plus")
+          }
+        }
+      }
+      TextField("Location",
+                text: $location)
+        .multilineTextAlignment(.center)
+        .frame(maxWidth: 100)
+        .padding(.vertical, 8)
+    }
   }
 }
-
-//Below is potentiall deprecated content
-//https://anthonycodesofficial.medium.com/swiftui-tutorial-how-to-create-a-floating-drop-down-menu-cc1562dbd48f
