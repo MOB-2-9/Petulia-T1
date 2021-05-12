2a15bedba8e35f9722b82b18f78d07fa47b68535
Henry Calderon <theceintiant@gmail.com>
2021-05-11 18:01:13 -0700

    Secondary breed now appears if the animal is mixed breed

--- Petulia/Views/DetailUI/PetDetailView.swift
+++ Petulia/Views/DetailUI/PetDetailView.swift
@@ -200,6 +200,15 @@ private extension PetDetailView {
           Text("\(viewModel.breed)")
           .font(.body)
           .fontWeight(.bold)
+        if viewModel.isMixed == true{
+          Text("Secondary breed: ")
+            .font(.body)
+            .fontWeight(.light)
+            +
+            Text("\(viewModel.breedSecond)")
+            .font(.body)
+            .fontWeight(.bold)
+        }
       }
       .padding(.top)
       
